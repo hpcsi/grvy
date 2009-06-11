f9717987bb083b263d6cfd4af060438d0ab950f8
Karl W. Schulz <karl@ices.utexas.edu>
2009-06-11 15:23:31 +0000

    hpct: updates to unit tests for input parser

--- test/input.c
+++ test/input.c
@@ -10,15 +10,24 @@
 
 const char *test_name = "C input parsing: var check";
 
+int compare_int    (int var1,    int var2);
+int compare_float  (float var1,  float var2);
+int compare_double (double var1, double var2);
+
 int main(int argc, char **argv)
 {
   float reyn,mach,aoa,A[3];
   int iter_max,turbulent;
   char *gridfile;
   char *key1, *key2;
+
+  int    ivar1, ivar2, ivec[4];
+  float  fvar1, fvar2, fvec[3];
+  double dvar1, dvar2, dvec[3];
+
   int flag = 1;
 
-  hpct_input_toggle_messages(0);
+  hpct_input_toggle_messages(1);
 
   //  printf("--> %s\n",test_name);
 
@@ -48,6 +57,47 @@ int main(int argc, char **argv)
   flag *= hpct_input_fread_char_ivec("solver/keywords",&key2,1);
   flag *= hpct_input_fread_float_vec("turbulence/A",A,3);
 
+  /* Read from the verify section and verify the results */
+
+  flag *= hpct_input_fread_int      ("verify/ivar",&ivar1);
+  flag *= hpct_input_fread_int_ivec ("verify/ivec",&ivar2,2);
+  flag *= hpct_input_fread_int_vec  ("verify/ivec",ivec,4);
+
+  if( ivec[0] != 11 || ivec[1] != 12 || ivec[2] != 13 || ivec[3] != 14)
+    {
+      flag = 0;
+      printf("\nRead int_ivec mismatch\n\n");
+    }
+
+  flag *= compare_int(ivar1,10);
+  flag *= compare_int(ivar2,13);
+
+  flag *= hpct_input_fread_float      ("verify/fvar",&fvar1);
+  flag *= hpct_input_fread_float_ivec ("verify/fvec",&fvar2,1);
+  flag *= hpct_input_fread_float_vec  ("verify/fvec",fvec,3);
+
+  if( fvec[0] != 101. || fvec[1] != 102. || fvec[2] != 103.)
+    {
+      flag = 0;
+      printf("\nRead float_ivec mismatch\n\n");
+    }
+
+  flag *= compare_float(fvar1,100.);
+  flag *= compare_float(fvar2,102.);
+
+  flag *= hpct_input_fread_double      ("verify/dvar",&dvar1);
+  flag *= hpct_input_fread_double_ivec ("verify/dvec",&dvar2,1);
+  flag *= hpct_input_fread_double_vec  ("verify/dvec",dvec,3);
+
+  if( dvec[0] != 1.1e20 || dvec[1] != 1.2e20 || dvec[2] != 1.3e20 )
+    {
+      flag = 0;
+      printf("\nRead double_ivec mismatch\n\n");
+    }
+
+  flag *= compare_double(dvar1,1.e20);
+  flag *= compare_double(dvar2,1.2e20);
+
   /* Close the file */
 
   flag *= hpct_input_fclose();
@@ -57,4 +107,34 @@ int main(int argc, char **argv)
   else 
     exit (0);
 
+}
+
+int compare_int(int var1,int var2)
+{
+  if(var1 != var2)
+    {
+      printf("\nRead variable mismatch: read %i, expecting %i\n\n",var1,var2);
+      return 0;
+    }
+  return 1;
+}
+
+int compare_float(float var1,float var2)
+{
+  if(var1 != var2)
+    {
+      printf("\nRead variable mismatch: read %f, expecting %f\n\n",var1,var2);
+      return 0;
+    }
+  return 1;
+}
+
+int compare_double(double var1,double var2)
+{
+  if(var1 != var2)
+    {
+      printf("\nRead variable mismatch: read %e, expecting %e\n\n",var1,var2);
+      return 0;
+    }
+  return 1;
 }
