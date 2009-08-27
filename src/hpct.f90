042391f33a1ba80c23452f39c182bbc79527d039
Karl W. Schulz <karl@ices.utexas.edu>
2009-08-27 04:21:29 +0000

    hpct: include pecos header

--- src/hpct.f90
+++ src/hpct.f90
@@ -1,6 +1,33 @@
-
+!--------------------------------------------------------------------------
+! -------------------------------------------------------------------------
+!
+! Copyright (C) 2008,2009 The PECOS Development Team
+!
+! Please see http:!pecos.ices.utexas.edu for more information.
+!
+! This file is part of the PECOS HPC Toolkit (HPCT)
+!
+! HPCT is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! HPCT is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with HPCT.  If not, see <http:!www.gnu.org/licenses/>.
+!
+! -------------------------------------------------------------------------
+!
+! hpct.f90: Fortran module interface definition for HPCT routines.
+!
+! $Id$
+! -------------------------------------------------------------------------
+! -------------------------------------------------------------------------
 module hpct
-!  use iso_c_binding
   interface
 
      ! ----------------------
