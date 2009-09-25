9821faebfe28c5a63dfc06d5d37fe525766ac6e2
Paul T. BAuman <pbauman@buffalo.edu>
2009-09-25 21:43:57 +0000

    hpct: Added C isnan and isinf tests to test directory. Should integrate with BuildBot now.

---
+++ test/math-isinf.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hpct.h"
+
+/*----------------------------------------------
+ * HPCT Test Suite
+ *
+ * $Id:  $
+ *----------------------------------------------*/
+
+const char *test_name = "C math: test isinf functionality";
+
+int main(int argc, char **argv)
+{
+
+  float  fone = 1.0;
+  double done = 1.0;
+
+  float  fzero = 0.0;
+  double dzero = 0.0;
+
+  float should_be_f_inf = fone/fzero;
+  float should_not_be_f_inf = 5.0;
+
+  double should_be_d_inf = done/dzero;
+  double should_not_be_d_inf = 6.0 ;
+
+  float *fval;
+  double *dval;
+
+  int is_my_val_inf = 0;
+
+  int flag = 0;
+
+  /* Make sure we get float inf */
+  fval = &should_be_f_inf;
+  is_my_val_inf = hpct_float_isinf( fval );
+  if( is_my_val_inf == 0 )
+    flag = 1;
+
+  /* Make sure we don't get a false positive */
+  fval = &should_not_be_f_inf;
+  is_my_val_inf = hpct_float_isinf( fval );
+  if( is_my_val_inf == 1 )
+    flag = 1;
+
+  /* Make sure we get double isnan */
+  dval = &should_be_d_inf;
+  is_my_val_inf = hpct_double_isinf( dval );
+  if( is_my_val_inf == 0 )
+    flag = 1;
+
+  /* Make sure we don't get a false positive */
+  dval = &should_not_be_d_inf;
+  is_my_val_inf = hpct_double_isinf( dval );
+  if( is_my_val_inf == 1 )
+    flag = 1;
+
+  if(flag == 0){
+      exit (0);}
+  else {
+    exit (1);}
+
+}
