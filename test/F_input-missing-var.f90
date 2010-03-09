b31f0c9ccda7df9ff02509996d21eaffbfcfeb65
Nicholas Malaya <nicholas.malaya@gmail.com>
2010-03-09 07:53:16 +0000

    almost certainly breaking hpct. I suppose I should express contrition

---
+++ test/F_input-missing-var.f90
@@ -0,0 +1,31 @@
+/*----------------------------------------------
+ * HPCT Test Suite
+ *
+ * F_input-missing-var.f90 8063 2010-03-08 nick
+ *----------------------------------------------*/
+
+program main
+  use hpct
+  implicit none
+
+  integer           :: ret_value = 1
+  real              :: reyn
+  character(len=60) :: filename
+
+
+  ! Silence warning/error/info message 
+  call hpct_log_setlevel(HPCT_NOLOG)
+  
+  filename = build_example_file_path()
+
+  ! Initialize/read the file
+  call hpct_input_fopen(filename,ret_value)
+  
+  ! Read specific variables and echo locally
+  hpct_input_fread_float("strangeways", reyn,ret_value)
+
+  ! Close the file
+  call hpct_input_fclose()
+
+  call exit(ret_value)
+end program main
---
+++ test/F_input-register.f90
@@ -0,0 +1,67 @@
+!*----------------------------------------------
+! * HPCT Test Suite
+! *
+! * Input variable registration
+! *
+! * $Id: input-register.f90 2422 2010-03-09 19:33:42Z nick
+! *----------------------------------------------
+
+
+program main
+  use hpct
+  implicit none
+
+  integer(4)        :: flag
+  integer(4)        :: iset = 123
+  real(4)           :: fset = 456.7
+  real(8)           :: dset = 123.e10
+  character(len=60) :: sset = "I am the very model of a modern Major-General"
+
+  ! Register variable (used for backwards-compatible input file support)
+
+  call hpct_input_register_int   ("sec1/aint",iset,flag)
+  call hpct_input_register_float ("sec1/afloat", fset,flag)
+  call hpct_input_register_double("sec1/adouble",dset,flag)
+  call hpct_input_register_char  ("sec1/astring",sset,flag)
+
+  ! Verify that we can recover what we registered as default
+
+  call hpct_input_register_get_int   ("sec1/aint",igot,flag)
+  call hpct_input_register_get_float ("sec1/afloat",fgot,flag)
+  call hpct_input_register_get_double("sec1/adouble",dgot,flag)
+  call hpct_input_register_get_char  ("sec1/astring",sgot,flag)
+
+  if(flag.eq.0) then
+     call exit(1)
+  endif
+
+  if(igot .ne. iset .or. fgot .ne. fset .or. dgot .ne. dset) then
+     call exit(1)
+  endif
+
+  ! Verify that we can recover registered default variables
+  ! even if they are not present in the input file
+  call hpct_log_setlevel(HPCT_NOLOG)
+
+  igot=-1;
+  fgot=-1.;
+  dgot=-1.;
+  sgot=NULL;
+
+  call hpct_input_fread_int   ("sec1/aint",igot,flag);
+  call hpct_input_fread_float ("sec1/afloat",fgot,flag);
+  call hpct_input_fread_double("sec1/adouble",dgot,flag);
+  call hpct_input_fread_char  ("sec1/astring",sgot,flag);
+  call hpct_input_fclose      ();
+
+  if(flag .eq. 0) then
+     exit(1);
+  endif
+
+  if(igot .ne. iset .or. fgot .ne. fset .or. dgot .ne. dset) then  
+     exit(1);
+  endif
+
+  return 0;
+  
+end program main
