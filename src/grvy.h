ee78439571aa036094a2b553a35322b4cb1cfd5c
Karl W. Schulz <karl@ices.utexas.edu>
2010-03-27 20:09:09 +0000

    libgrvy: expanded doxygen commentary to include mention of logging mechanism. Also added some other routines which had not been included previously (variable registration functions and dump capabilities) - Issue #784.

--- src/grvy.h
+++ src/grvy.h
@@ -146,8 +146,6 @@ extern "C" {
   extern int    grvy_create_unique_dir    (      char *name_template);
   extern int    grvy_create_scratch_dir   (      char *name_template);
 
-
-
   /*
    * Doxygen API Definitions
    */
@@ -155,49 +153,160 @@ extern "C" {
   /*! \page apiC
    * \section input Input Parsing Routines
    *
-   * <b>Open/close GRVY input file:</b>
+   * <b>Open/close a libGRVY style input file:</b>
    * \code
-   * int grvy_input_fopen             (const char *filename);
-   * int grvy_input_fclose            ();
+   * int grvy_input_fopen               (const char *filename);
+   * int grvy_input_fclose              ();
    * \endcode
    *
-   * <b>Read a single int, float, or double for input keyword \e var:</b>
+   * <b> Dump the contents of a libGRVY style input file :</b>
+   * 
+   * The following routines can be used to dump the entire contents of
+   * a parsed libGRVY style input file to \e stdout or to an ASCII
+   * file (note that all comments within the original input file are
+   * stripped). The prefix variable can be used to include an
+   * additional delimiter which prepends each line of the output.
+   * These routines are normally used to save all relevant input
+   * parameters in the application output for future traceability and
+   * repeatability.
+   *
    * \code
-   * int grvy_input_fread_int         (const char* var, int    *value);
-   * int grvy_input_fread_float       (const char* var, float  *value);
-   * int grvy_input_fread_double      (const char* var, double *value);
+   * int grvy_input_fdump               ();
+   * int grvy_input_fdump_delim         (const char* prefix);
+   * int grvy_input_fdump_file          (const char* prefix, const char *filename);
    * \endcode
    *
-   * <b>Read a vector of  ints, floats, or doubles for input keyword \e var:</b>
+   * <b>Read a single int, float, double, or char string for input keyword \e var:</b>
    * \code
-   * int grvy_input_fread_int_vec     (const char* var, int    *value, int nelems);
-   * int grvy_input_fread_float_vec   (const char* var, float  *value, int nelems);
-   * int grvy_input_fread_double_vec  (const char* var, double *value, int nelems);
+   * int grvy_input_fread_int           (const char* var, int    *value);
+   * int grvy_input_fread_float         (const char* var, float  *value);
+   * int grvy_input_fread_double        (const char* var, double *value);
+   * int grvy_input_fread_char          (const char* var, char  **value);
+   * \endcode
+   *
+   * <b>Read a vector of ints, floats, or doubles for input keyword \e var:</b>
+   * \code
+   * int grvy_input_fread_int_vec       (const char* var, int    *value, int nelems);
+   * int grvy_input_fread_float_vec     (const char* var, float  *value, int nelems);
+   * int grvy_input_fread_double_vec    (const char* var, double *value, int nelems);
+   * \endcode
+   *
+   * <b>Read the \e ith entry (elem) of a vector of ints, floats,
+   * doubles, or characters for input keyword \e var:</b>
+   *
+   * \code
+   * int grvy_input_fread_int_ivec      (const char* var, int *value,    int elem);
+   * int grvy_input_fread_float_ivec    (const char *var, float *value,  int elem);
+   * int grvy_input_fread_double_ivec   (const char* var, double *value, int elem);
+   * int grvy_input_fread_char_ivec     (const char* var, char **value,  int elem);
+   * \endcode
+   *
+   * <b> Support for the creation of backwards compatible input files:</b>
+   *
+   * The following routines provide a mechanism to register a default
+   * value with the input parsing mechanism.  If a particular keyword
+   * (\e var) has a registered default value and is subsequently
+   * queried with an input file which does not contain the keyword, the
+   * registered default value will be returned instead.  A convenient
+   * use for these registration functions is to provide sensible
+   * defaults to newly added input keywords which are not present in
+   * older input file revisions.
+   *
+   * \code
+   * int grvy_input_register_int        (const char *var,     int value);
+   * int grvy_input_register_float      (const char *var,   float value);
+   * int grvy_input_register_double     (const char *var,  double value);
+   * int grvy_input_register_char       (const char *var,   char *value);
+   * \endcode
+   *
+   * The following routines can be used to query the current hashed
+   * values of any keyword variables which have been registered
+   * previously.
+   *
+   * \code
+   * int grvy_input_register_get_int    (const char *var,    int *value);
+   * int grvy_input_register_get_float  (const char *var,  float *value);
+   * int grvy_input_register_get_double (const char *var, double *value);
+   * int grvy_input_register_get_char   (const char *var,  char **value);
    * \endcode
    *
    * \section timing Performance Timing Routines
    * \code
-   * double grvy_timer                ();
-   * void   grvy_timer_init           (const char *id);
-   * void   grvy_timer_reset          ();
-   * void   grvy_timer_finalize       ();
-   * void   grvy_timer_begin          (const char *id);
-   * void   grvy_timer_end            (const char *id);
-   * double grvy_timer_elapsedseconds (const char *id);
-   * double grvy_timer_elapsed_global ();
-   * void   grvy_timer_summarize      ();
+   * double grvy_timer                  ();
+   * void   grvy_timer_init             (const char *id);
+   * void   grvy_timer_reset            ();
+   * void   grvy_timer_finalize         ();
+   * void   grvy_timer_begin            (const char *id);
+   * void   grvy_timer_end              (const char *id);
+   * double grvy_timer_elapsedseconds   (const char *id);
+   * double grvy_timer_elapsed_global   ();
+   * void   grvy_timer_summarize        ();
+   * \endcode
+
+   * \section logging Logging Utilities
+   *
+   * The following routines provide a priority-based logging mechanism
+   * to control application messages.  A set of default priority definitions are
+   * provided to define messages of the following form:
+   *
+   * - GRVY_FATAL
+   * - GRVY_ERROR
+   * - GRVY_WARN 
+   * - GRVY_INFO 
+   * - GRVY_DEBUG
+   *
+   * The suggested usage of the logging mechanism is to use the
+   * following routine to generate messages as opposed to writing to \e stdout
+   * or \e stderr directly.  Note that to use, each message must also be assigned a 
+   * priority loglevel (e.g. GRVY_WARN).
+   *
+   * \code void grvy_log                    (int loglevel, const char *mesg); \endcode
+   * 
+   * The verbosity of the application output can then be controlled by
+   * setting the current loglevel as desired (for example, the GRVY_INFO
+   * mode would show all messages except those set to the GRVY_DEBUG
+   * loglevel).
+   *
+   * \code void grvy_log                    (int loglevel, const char *mesg); \endcode
+   *
+   * Note that two additional default priority loglevels exist,
+   * GRVY_NOLOG and GRVY_ALL which can be used to disable or enable
+   * all log output, respectively.
+   *
+   * \section math Math Utilities
+   * 
+   * The following routines provide unified floating-point checks for NANs and INFs.
+   *
+   * \code
+   * int grvy_double_isnan              (double *val);
+   * int grvy_double_isinf              (double *val);
+   *   				  
+   * int grvy_float_isnan               (float  *val);
+   * int grvy_float_isinf               (float  *val);
    * \endcode
    *
    * \section misc Miscellaneous Routines
-   * <b>grvy_check_file_path: Check desired output \e pathname and create any non-existing parent directories.</b>
+   * <b>grvy_check_file_path:</b>
+   * This routine checks a desired output \e pathname and creates any non-existing parent directories.
    * \code
-   * int    grvy_check_file_path      (const char *pathname);
+   * int    grvy_check_file_path        (const char *pathname);
    * \endcode
-   * <b>grvy_create_unique_dir: Create a new, unique directory with 0700 permissions using the provided template name.  The last six characters of name_template must be XXXXXX, and name_template is modified with the unique name upon return.</b>
+   * <b>grvy_create_unique_dir:</b>
+   *
+   * This routine creates a new, unique directory with 0700 permissions using the
+   * provided template name.  The last six characters of name_template
+   * must be XXXXXX, and name_template is modified with the unique
+   * name upon return.
+   *
    * \code
    * int    grvy_create_unique_dir      (char *name_template);
    * \endcode
-   * <b>grvy_create_scratch_dir: Identical to grvy_create_unique_dir, except the new directory and all of its contents are deleted when the process exits.</b>
+   *
+   * <b>grvy_create_scratch_dir:</b>
+   *
+   * Identical to grvy_create_unique_dir(), except that the new directory and
+   * all of its contents are deleted when the process exits.
+   *
    * \code
    * int    grvy_create_scratch_dir     (char *name_template);
    * \endcode
