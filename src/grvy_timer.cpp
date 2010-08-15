840d4073f038e1d1992c4b14562b9cc14e2c6140
Karl W. Schulz <karl@ices.utexas.edu>
2010-08-15 14:02:03 +0000

    [grvy]: changed license to LGPL 2.1

--- src/grvy_timer.cpp
+++ src/grvy_timer.cpp
@@ -2,26 +2,26 @@
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
 //
+// libGRVY - a utility library for scientific computing.
+//
 // Copyright (C) 2008,2009,2010 The PECOS Development Team
 //
-// Please see http://pecos.ices.utexas.edu for more information.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
 //
-// This file is part of the PECOS GRVY Toolkit
-//
-// GRVY is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// GRVY is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with GRVY.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
 //
 //--------------------------------------------------------------------------
+//
 // grvy_timer: Timing routines for performance monitoring
 //
 // $Id$
