e817ea61353ff9141b48de68c3cd38a0e1ca4bb2
Karl W. Schulz <karl@ices.utexas.edu>
2010-12-07 20:12:18 +0000

    [grvy]: update example to current API

--- examples/hdf5_access.cpp
+++ examples/hdf5_access.cpp
@@ -80,7 +80,7 @@ int main(int argc, char **argv)
   gt.Finalize();
   gt.Summarize();
 
-  gt.SaveHistTiming("Example1","",1,"koomie2.h5");
+  gt.SaveHistTiming("Example1","",1,1,-1,"koomie2.h5");
 
   // Query the historical performance logs
 
