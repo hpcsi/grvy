8ca4fb9496765760250c876eb66e9748daa77417
Karl W. Schulz <karl@ices.utexas.edu>
2010-12-13 02:00:08 +0000

    [grvy]: starting hdf regression

---
+++ test/C++_hdf_hist_timing.cpp
@@ -0,0 +1,73 @@
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//
+// libGRVY - a utility library for scientific computing.
+//
+// Copyright (C) 2008,2009,2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//--------------------------------------------------------------------------
+//
+// Regression/Test Suite for libGRVY.
+//
+// $Id: hdf5_access.cpp 16240 2010-12-10 17:00:12Z karl $
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<iostream>
+#include<grvy.h>
+
+using namespace GRVY;
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+  GRVY_Timer_Class gt;	// GRVY performance timer
+  
+  gt.Init("mytimer");
+  gt.BeginTimer("Main Program");
+  
+  grvy_log_setlevel(GRVY_NOLOG);
+
+  // do some busy work
+
+  double foo = 0;
+
+  gt.BeginTimer("Work");
+
+  for(int i=0;i<1000000;i++)
+    foo = foo + i*foo*3.1415*sqrt(foo)*exp(foo)/foo;
+
+  gt.EndTimer("Work");
+
+  // Finalize the performance timer
+
+  gt.EndTimer("Main Program");
+  gt.Finalize();
+  gt.Summarize();
+
+  gt.SaveHistTiming("Example1","",1,1,-1,-1,"test.h5");
+
+  // Query the historical performance logs
+
+  gt.SummarizeHistTiming("test.h5","#","ghist");
+
+  return 0;
+}
