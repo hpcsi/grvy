9095333411706c98870424815701ac7990ac5485
Karl W. Schulz <karl@ices.utexas.edu>
2011-05-26 18:35:17 +0000

    [grvy]: testing app for ocore

---
+++ src/example.cpp
@@ -0,0 +1,14 @@
+#include<grvy.h>
+
+using namespace std;
+using namespace GRVY;
+
+int main()
+{
+  GRVY_MPI_Ocore_Class ocore;
+
+  string inputfile("myfile");
+
+  ocore.Initialize(inputfile,8192);
+
+}
