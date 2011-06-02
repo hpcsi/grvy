f405e202b17fc579bd27c10b4cb00345b3e6f29d
Karl W. Schulz <karl@ices.utexas.edu>
2011-06-02 03:10:59 +0000

    [grvy]: update/clean fortran ocore example

--- src/example3.f90
+++ src/example3.f90
@@ -1,16 +1,46 @@
+!!--------------------------------------------------------------------------
+!!--------------------------------------------------------------------------
+!!
+!! libGRVY - a utility library for scientific computing.
+!!
+!! Copyright (C) 2008,2009,2010 The PECOS Development Team
+!!
+!! This library is free software; you can redistribute it and/or
+!! modify it under the terms of the Version 2.1 GNU Lesser General
+!! Public License as published by the Free Software Foundation.
+!!
+!! This library is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+!! Lesser General Public License for more details.
+!!
+!! You should have received a copy of the GNU Lesser General Public
+!! License along with this library; if not, write to the Free Software
+!! Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+!! Boston, MA  02110-1301  USA
+!!
+!!--------------------------------------------------------------------------
+!!
+!! ocoref.f90: Fortran example illustrating MPI out-of-core usage.
+!!
+!! $Id$
+!!--------------------------------------------------------------------------
+!!--------------------------------------------------------------------------
 
 program main
   use grvy
   implicit none
 
-  integer,parameter   :: blocksize = 8192;
-  real*8              :: data(blocksize)
-  integer             :: num_records = 800
+  integer,parameter   :: blocksize = 8192; ! number of elements per ocore record
+  real*8              :: data(blocksize)   ! individual data record
+  integer             :: num_records = 800 ! # of records defined in this example
 
-  integer             :: i
-  integer             :: ierr
+  integer             :: ierr              ! Ocore return code
   integer*8           :: elem
-  integer             :: verify_data
+  integer             :: verify_data 
+
+  ! Initialize MPI-based Ocore ramdisk (myfile is a GRVY-style
+  ! input file with optional Ocore controls
 
   call grvy_ocore_init("myfile",blocksize,ierr)
 
@@ -18,24 +48,29 @@ program main
      print*,'Unable to initialize GRVY Ocore'
      stop
   endif
+
+  ! Do some work on the master task - children tasks poll until
+  ! read/write requests are made from the master task
   
    if(grvy_ocore_master() .eq. 1)then
 
-      ! Store some known data
+      ! Write some known data records to Ocore ramdisk - the
+      ! grvy_ocore_write() function distribute the data over the
+      ! network to children Ocore tasks which cache the data in
+      ! memory. Once the memory cache is full, it will (optionally)
+      ! dump less-freqently access records to a disk-based temporary
+      ! file.
 
       do elem = 1,num_records
-         i = elem
-         call init_data(i,blocksize,data)
+         call init_data       (elem,blocksize,data)
          call grvy_ocore_write(elem,data,ierr)
       enddo
 
-      ! Change data locally
+      ! Change the local data record
 
-      do i = 1,num_records
-         call zero_data(i,blocksize,data)
-      enddo
-
-      ! Read back known data in opposite order and verify
+      data(:) = 0.0d0
+      
+      ! Read back known data records in opposite order and verify
 
       do elem = num_records,1,-1
          call grvy_ocore_read(elem,data,ierr);
@@ -50,58 +85,67 @@ program main
 
   stop
 end program main
-  
 
-subroutine init_data(offset,size,data)
+! -------------------------------------------------------
+! init_data(): initialize all records to a known state 
+!              based on given seed value
+! -------------------------------------------------------
+
+subroutine init_data(seed,size,data)
   implicit none
-  integer, intent(in)  :: offset
-  integer, intent(in)  :: size
-  real*8,  intent(out) :: data(*)
+  integer*8, intent(in)  :: seed
+  integer,   intent(in)  :: size
+  real*8,    intent(out) :: data(size)
 
   integer :: i
 
   do i = 1,size
-     data(i) = offset*i
+     data(i) = seed*i
   enddo
 
   return
 end subroutine init_data
 
-function verify_data(offset,size,data)
+! -------------------------------------------------------
+! verify_data(): verify all records against known state 
+!                based on given seed value
+! -------------------------------------------------------
+
+function verify_data(seed,size,data)
   implicit none
 
   integer verify_data
 
-  integer, intent(in)  :: offset
-  integer, intent(in)  :: size
-  real*8,  intent(out) :: data(size)
+  integer*8, intent(in)  :: seed
+  integer,   intent(in)  :: size
+  real*8,    intent(out) :: data(size)
 
   integer :: i
 
   do i = 1,size
-     if( abs(data(i) - 1.0d0*offset*i) .gt.  1.0d-15)then
-        print*,'Verification error: expecting ',1.0d0*offset*i,' but found ',data(i),' for index ',i
+     if( abs(data(i) - 1.0d0*seed*i) .gt.  1.0d-15)then
+        print*,'Verification error: expecting ',1.0d0*seed*i,' but found ',data(i),' for index ',i
         verify_data = 0
         return 
      endif
   enddo
 
   verify_data = 1
+
   return
 end function verify_data
 
-subroutine zero_data(offset, size, data)
+! -------------------------------------------------------
+! zero_data(): initialize all records to zero
+! -------------------------------------------------------
+
+subroutine zero_data(size, data)
   implicit none
 
-  integer, intent(in)  :: offset
   integer, intent(in)  :: size
   real*8,  intent(out) :: data(size)
 
-  integer :: i
-
-  do i =1,size
-     data(i) = 0.0d0
-  enddo
+  data(:) = 0.0d0
 
   return
 end subroutine zero_data
