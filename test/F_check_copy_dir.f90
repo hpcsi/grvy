b8c67dc149ff185e598befdd7192a15034abb54d
Karl W. Schulz <karl@ices.utexas.edu>
2011-10-27 11:50:08 +0000

    [grvy]: final fortran regression for grvy_copy_dir - Issue #2089

--- test/F_check_copy_dir.f90
+++ test/F_check_copy_dir.f90
@@ -31,52 +31,82 @@ program F_check_copy_dir
   use grvy
   implicit none
 
-!  character*14 :: template1  = "tmpdirf_XXXXXX"
-!  character*14 :: template2  = "tmpdirf_XXXXXX"
+  character*14 :: template1  = "tmpdirf_XXXXXX"
+  character*14 :: template2  = "tmpdirf_XXXXXX"
 
-  integer :: flag = 1
-  integer :: retval
+  integer :: SUCCESS = 0
+  integer :: FAILURE = 1
 
-  character*34 :: valid_template   = "TempDir_itHasSixTrailingExesXXXXXX"
+  integer :: flag
+  integer :: status
 
-!!!  call grvy_log_setlevel(GRVY_NOLOG)
+  call grvy_log_setlevel(GRVY_NOLOG)
 
-  print*,'flag = ',flag
+ call grvy_create_unique_dir(template1, flag)
+  if(flag .ne. SUCCESS)then
+    write (*,*) "Create unique_dir unsuccessful ",template1
+    call exit(1)
+ endif
 
-  call grvy_create_unique_dir(valid_template, retval)
-  print*,'retval = ',retval
-  flag = flag * retval
-
-  print*,'flag = ',flag
-
-!  call grvy_create_unique_dir(template1,  retval)
-!  flag = flag * retval
-
-!  print*,'retval = ',retval
-!  print*,'flag = ',flag
-
-!  call grvy_create_unique_dir(template2, retval)
-!  flag = flag * retval
+ call grvy_create_unique_dir(template2, flag)
+  if(flag .ne. SUCCESS)then
+    write (*,*) "Create unique_dir unsuccessful",template2
+    call exit(1)
+ endif
 
   ! Failure should occur if we attempt to copy a file 
 
-!   call grvy_copy_dir("F_check_file_path",template,retval)
-!   if(retval .ne. 1)then
-!      flag = 0
-!   endif
+ call grvy_copy_dir("F_check_file_path",template1,flag)
+ if(flag .ne. FAILURE)then
+    write (*,*) "Expected failure for file copy"
+    call exit(1)
+ endif
 
-!   ! Failure should occur if input/destination dirs are identical 
+ ! Failure should occur if input/destination dirs are identical 
 
-!   call grvy_copy_dir(template,template,retval)
-!   if(retval .ne. 1)then
-!      flag = 0
-!   endif
+ call grvy_copy_dir(template1,template1,flag)
+ if(flag .ne. FAILURE)then
+    write (*,*) "Expected failure for identical in/out dirs"
+    call exit(1)
+ endif
 
-  if(flag .eq. 0)then
-     retval = 1
-  else
-     retval = 0
-  endif
+ ! Success should occur for existing directory copied to empty existing destination 
 
-  call exit(retval)
+ call grvy_copy_dir("ref_files/adir",template1,flag);
+ if(flag .ne. SUCCESS)then
+    write (*,*) "copy_dir to empty directory unsuccesful"
+    call exit(1)
+ endif
+ 
+ ! verify result
+ 
+ status = system ("./diff_dir.sh ref_files/adir "//template1)
+ if(status .ne. 0)then
+    write (*,*) "copy_dir to empty directory does not verify"
+    call exit(1)
+ endif
+
+ ! Success should occur for existing directory copied to non-existing destination dir
+
+ status = system ("rmdir "//template2)
+ if(status .ne. 0)then
+    write (*,*) "unable to remove existing dir",template2
+    call exit(1)
+ endif
+
+ call grvy_copy_dir("ref_files/adir",template2,flag);
+ if(flag .ne. SUCCESS)then
+    write (*,*) "copy_dir to non-existent directory unsuccesful"
+    call exit(1)
+ endif
+ 
+ ! verify result
+ 
+ status = system ("./diff_dir.sh ref_files/adir "//template2)
+ if(status .ne. 0)then
+    write (*,*) "copy_dir to non-existent directory does not verify"
+    call exit(1)
+ endif
+
+  call exit(0)
 end program F_check_copy_dir
