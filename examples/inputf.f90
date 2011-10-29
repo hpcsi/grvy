a1b9f61915b8726d44eb9e5330518a8e1cc8dfd9
Karl W. Schulz <karl@ices.utexas.edu>
2011-10-29 00:17:29 +0000

    [grvy]: include boost system - looks to be required with boost filesystem; include boolean in example

--- examples/inputf.f90
+++ examples/inputf.f90
@@ -39,7 +39,6 @@ program main
   character            :: key1*100
   character            :: key2*100
   logical              :: restart
-  logical :: restart2
 
   ! Initialize/read the file
 
@@ -77,10 +76,9 @@ program main
   print*,'solver/keyword 1    = ',trim(key1)
   print*,'solver/keyword 2    = ',trim(key2)
 
-  ! Read a logical (you need to provide a default which is used if the
-  ! keyword is not specified in the input file)
+  ! Read a logical (you need to provide a default value which is used if the
+  ! desired keyword is not available in the input file)
 
-  restart2 = .false.
   call grvy_input_fread_logical  ("restart",restart,.false.,flag)
 
   print*,'restart = ',restart
