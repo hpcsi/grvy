c78c389838605e3b65ef5adeb193577bf15b22f8
Karl W. Schulz <karl@ices.utexas.edu>
2012-04-19 04:34:13 +0000

    [grvy]: additional regressions testing lack of Init() or Finalize() calls for performance timers - Issue #1690

---
+++ test/C_timer_errors.c
@@ -0,0 +1,178 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// libGRVY - a utility library for scientific computing.
+//
+// Copyright (C) 2008,2009,2010,2011,2012 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+//
+// Regression/Test Suite for libGRVY.
+//
+// $Id$
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include<grvy.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<time.h>
+#include<math.h>
+#include <sys/time.h>
+
+double Foo_Sleep = 0.098 * 1.e6;
+double Bar_Sleep = 0.075 * 1.e6;
+double boo_Sleep = 0.23  * 1.e6;
+double Max_Iters = 3;
+double Tolerance = 5e-4;	/* a coarse-grained check to compare against gettimeofday() */
+
+void foo();
+void bar();
+void boo();
+
+double gtod_total_timing = 0.0;
+double boo_gtod_timing   = 0.0;
+
+struct timeval tv;
+
+int main()
+{
+
+  int i,itest;
+  int num_repeat = 2;
+  double t1;
+  int flag;
+
+  // Verify error if no initialization
+
+  /* Primary Iteration Loop */
+
+  gettimeofday(&tv,NULL);
+  t1 =  ((double) tv.tv_sec) + 1.e-6*((double) tv.tv_usec);
+
+  grvy_timer_init("GRVY");
+  raw_grvy_timer = grvy_timer();
+  
+  /* Do some work - note that foo() includes calls to other
+   * routines to test embedded timer capability.  */
+  
+  for(itest=0;itest<num_repeat;itest++)
+    {
+      for(i=0;i<Max_Iters;i++)
+	foo();
+    }
+
+  grvy_total_timing = grvy_timer_elapsed_global();
+  grvy_boo_timing   = grvy_timer_elapsedseconds("boo");
+
+  gettimeofday(&tv,NULL);
+  gtod_total_timing +=   ((double) tv.tv_sec) + 1.e-6*((double) tv.tv_usec) - t1;
+
+  grvy_timer_finalize();
+  raw_grvy_timer = grvy_timer() - raw_grvy_timer;
+
+
+  double diff = fabs(grvy_boo_timing-boo_gtod_timing);
+
+  if( diff > Tolerance)
+    {
+      grvy_printf(GRVY_ERROR,"Function timing mismatch -> diff = %e (secs)\n",diff);
+      grvy_printf(GRVY_ERROR,"The test host could be overloaded or the timer may be incorrect\n");
+      return(1);
+    }
+
+  diff = fabs(grvy_total_timing-gtod_total_timing);
+
+  if( diff > Tolerance)
+    {
+      grvy_printf(GRVY_ERROR,"Global timing mismatch -> diff = %e (secs)\n",diff);
+      grvy_printf(GRVY_ERROR,"The test host could be overloaded or the timer may be incorrect\n");
+      return(1);
+    }
+
+  diff = fabs(grvy_total_timing-raw_grvy_timer);
+
+  if( diff > Tolerance)
+    {
+      grvy_printf(GRVY_ERROR,"Internal global timing mismatch -> diff = %e (secs)\n",diff);
+      grvy_printf(GRVY_ERROR,"The test host could be overloaded or the timer may be incorrect\n");
+      return(1);
+    }
+
+  // Test a reset()
+
+  grvy_timer_reset();
+
+  grvy_total_timing = grvy_timer_elapsed_global();
+  grvy_boo_timing   = grvy_timer_elapsedseconds("boo");
+
+  if( (grvy_total_timing > Tolerance) || (grvy_boo_timing != 0.0) )
+    {
+      grvy_printf(GRVY_ERROR,"Problem with timer reset\n");
+      grvy_printf(GRVY_ERROR," --> total timing = %f\n",grvy_total_timing);
+      grvy_printf(GRVY_ERROR," -->   boo timing = %f\n",grvy_boo_timing);
+      return(1);
+    }
+
+  return 0;
+}
+
+
+void foo()
+{
+  grvy_timer_begin(__func__);
+
+  usleep(Foo_Sleep);
+  bar();
+
+  grvy_timer_end(__func__);
+  return;
+}
+
+void bar()
+{
+  grvy_timer_begin(__func__);
+
+  usleep(Bar_Sleep);
+  boo();
+
+  grvy_timer_end(__func__);
+
+  return;
+}
+
+
+void boo()
+{
+  static double t1;
+
+  gettimeofday(&tv,NULL);
+  t1 =  ((double) tv.tv_sec) + 1.e-6*((double) tv.tv_usec);
+
+  grvy_timer_begin(__func__);
+
+  usleep(boo_Sleep);
+
+  grvy_timer_end(__func__);
+
+  gettimeofday(&tv,NULL);
+  boo_gtod_timing +=   ((double) tv.tv_sec) + 1.e-6*((double) tv.tv_usec) - t1;
+
+  return;
+    
+}
---
+++ test/C_timer_no_init.c
@@ -0,0 +1,169 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// libGRVY - a utility library for scientific computing.
+//
+// Copyright (C) 2008,2009,2010,2011,2012 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+//
+// Regression/Test Suite for libGRVY.
+//
+// $Id$
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include<grvy.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<math.h>
+
+const double Foo_Sleep = 0.03 * 1.e6;
+const double Bar_Sleep = 0.05 * 1.e6;
+const double Boo_Sleep = 0.1  * 1.e6;
+const double Max_Iters = 3;
+
+const double Rel_TOL   = 0.1;	// tolerance (coarse-grained for potentially busy test hosts)
+
+void   foo();
+void   bar();
+void   boo();
+double vette_relative_diff(char *id, double expected);
+
+
+int main()
+{
+
+  int i,itest;
+  int num_repeat = 2;
+
+  /* Do some work - note that foo() includes calls to other
+   * routines to test embedded timer capability.  */
+  
+  for(itest=0;itest<num_repeat;itest++)
+    {
+      for(i=0;i<Max_Iters;i++)
+	foo();
+    }
+
+  grvy_timer_finalize();
+  //grvy_timer_summarize();
+
+  double Boo_expected = Boo_Sleep*Max_Iters*num_repeat/1.e6;
+  double Bar_expected = Bar_Sleep*Max_Iters*num_repeat/1.e6;
+  double Foo_expected = Foo_Sleep*Max_Iters*num_repeat/1.e6;
+  double total_expected = Boo_expected + Bar_expected + Foo_expected;
+
+  grvy_printf(GRVY_DEBUG,"Expected time for boo = %e secs\n",Boo_expected);
+  grvy_printf(GRVY_DEBUG,"Expected time for bar = %e secs\n",Bar_expected);
+  grvy_printf(GRVY_DEBUG,"Expected time for foo = %e secs\n",Foo_expected);
+
+  /* Timer values reasonable? */
+
+  vette_relative_diff("boo",Boo_expected);
+  vette_relative_diff("bar",Bar_expected);
+  vette_relative_diff("foo",Foo_expected);
+
+  /* Total time reasonable? */
+
+  double total_time = grvy_timer_elapsed_global();
+
+  if(total_time <= 0.0)
+    exit(1);
+
+  if(fabs(total_time - total_expected)/total_expected > Rel_TOL)
+    {
+      grvy_printf(GRVY_ERROR,"Global timer mismatch - test host may be overloaded\n");
+      exit(1);
+    }
+
+  /* Unassigned timer reasonable? */
+
+  double unassigned = total_time - ( grvy_timer_elapsedseconds("foo") + 
+				     grvy_timer_elapsedseconds("boo") + 
+				     grvy_timer_elapsedseconds("bar") );
+
+  if(unassigned <= 0.0)
+    exit(1);
+  
+  if(unassigned > 1e-3)
+    {
+      grvy_printf(GRVY_ERROR,"Unassigned timer mismatch - test host may be overloaded\n");
+      grvy_printf(GRVY_ERROR,"--> Unaccounted time = %e\n",unassigned);
+      exit(1);
+    }
+
+  return 0;
+}
+
+
+void foo()
+{
+  grvy_timer_begin(__func__);
+
+  usleep(Foo_Sleep);
+  bar();
+
+  grvy_timer_end(__func__);
+  return;
+}
+
+void bar()
+{
+  grvy_timer_begin(__func__);
+
+  usleep(Bar_Sleep);
+  boo();
+
+  grvy_timer_end(__func__);
+
+  return;
+}
+
+
+void boo()
+{
+  grvy_timer_begin(__func__);
+
+  usleep(Boo_Sleep);
+
+  grvy_timer_end(__func__);
+
+  return;
+    
+}
+
+double vette_relative_diff(char *id,double expected)
+{
+  double measured = grvy_timer_elapsedseconds(id);
+
+  if(measured <= 0.0)
+    {
+      grvy_printf(GRVY_ERROR,"Elapsed time should be > 0 (%s)\n",id);
+      exit(1);
+    }
+
+  if(fabs(measured-expected)/expected > Rel_TOL)
+    {
+      grvy_printf(GRVY_INFO,"Timer mismatch - test host may be overloaded\n");
+      grvy_printf(GRVY_INFO,"  --> Measured      = %e\n",measured);
+      grvy_printf(GRVY_INFO,"  --> Expected      = %e\n",expected);
+      grvy_printf(GRVY_INFO,"  --> Relative Diff = %e (%)\n",fabs(measured-expected)/expected);
+    }
+
+}
