4b8a3cf0e6f003f6bf8131f9fa10d8695df4aad7
Karl W. Schulz <karl@ices.utexas.edu>
2018-10-08 10:50:31 -0500

    header updates

--- test/F_ocore.f90
+++ test/F_ocore.f90
@@ -1,26 +1,26 @@
-//-----------------------------------------------------------------------bl-
-//--------------------------------------------------------------------------
-// 
-// libGRVY - a utility library for scientific computing.
-//
-// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
-// Additional Copyright (C) 2018 individual authors
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the Version 2.1 GNU Lesser General
-// Public License as published by the Free Software Foundation.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
-// Boston, MA  02110-1301  USA
-//
-//-----------------------------------------------------------------------el-
+!!-----------------------------------------------------------------------bl-
+!!--------------------------------------------------------------------------
+!! 
+!! libGRVY - a utility library for scientific computing.
+!!
+!! Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+!! Additional Copyright (C) 2018 individual authors
+!!
+!! This library is free software; you can redistribute it and/or
+!! modify it under the terms of the Version 2.1 GNU Lesser General
+!! Public License as published by the Free Software Foundation.
+!!
+!! This library is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+!! Lesser General Public License for more details.
+!!
+!! You should have received a copy of the GNU Lesser General Public
+!! License along with this library; if not, write to the Free Software
+!! Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+!! Boston, MA  02110-1301  USA
+!!
+!!-----------------------------------------------------------------------el-
 !!
 !! ocore_f.f90: Fortran example illustrating MPI out-of-core usage.
 !!
